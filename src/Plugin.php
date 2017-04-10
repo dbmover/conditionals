331f74333f11c5a3f5ba2488b94bf1a4846e2c85
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-10 16:41:12 +0200

    add description and use new-style defer/addOperation

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,14 +13,16 @@ use Dbmover\Core;
 
 class Plugin extends Core\Plugin
 {
+    public $description = 'Executing conditional blocks...';
+
     public function __invoke(string $sql) : string
     {
         if (preg_match_all('@^IF.*?^END IF;$@ms', $sql, $ifs, PREG_SET_ORDER)) {
             foreach ($ifs as $if) {
                 $tmp = 'tmp_'.md5(microtime(true));
                 $code = $this->wrap($if[0]);
-                $this->defer($code, $if[0]);
-                $this->loader->addOperation($code, $if[0]);
+                $this->defer($code);
+                $this->addOperation($code);
                 $sql = str_replace($if[0], '', $sql);
             }
         }
